26de3ded27527ef4758a6bd58abe8d6ab1ee81bf
Khusroo Hayat <khusroo.hayat@gmail.com>
2025-10-20 17:39:27 +0500

    logo section component created and added new logo

---
+++ app/TechLogosMarquee.js
@@ -0,0 +1,59 @@
+export default function TechLogosMarquee() {
+  return (
+    <section className="logos container">
+      <div className="marquee">
+        <div className="track">
+          <img src="./imgs/vscode.png" width="128" alt="VS Code" title="VS Code" />
+          <img src="./imgs/cursor.png" width="128" alt="Cursor" title="Cursor" />
+          <img src="./imgs/windsurf.png" width="128" alt="Windsurf" title="Windsurf" />
+          <img src="./imgs/lovable.png" width="128" alt="Lovable" title="Lovable" />
+          <img src="./imgs/replit.png" width="128" alt="Replit" title="Replit" />
+          <img src="./imgs/v0.png" width="128" alt="v0" title="v0" />
+          <img
+            src="./imgs/firebase-studio.png"
+            width="128"
+            alt="Firebase Studio"
+            title="Firebase Studio"
+          />
+          <img src="./imgs/asp-net.png" alt="Asp.net" width="128" title="Asp.net" />
+          <img src="./imgs/vue-js-logo.png" alt="Vue.js" width="128" title="Vue.js" />
+          <img src="./imgs/azure.png" width="128" alt="Azure" title="Azure" />
+          <img src="./imgs/node-js.png" width="128" alt="Node.js" title="Node.js" />
+          <img src="./imgs/typescript.png" width="128" alt="TypeScript" title="TypeScript" />
+          <img src="./imgs/sql-server.png" width="128" alt="SQL Server" title="SQL Server" />
+          <img src="./imgs/chatgpt.png" width="128" alt="ChatGPT" title="ChatGPT" />
+          <img src="./imgs/mongodb.png" alt="MongoDB" width="128" title="MongoDB" />
+          <img src="./imgs/blazor.png" alt="Blazor" width="128" title="Blazor" />
+          <img src="./imgs/nextjs.png" width="128" alt="Next JS" title="Next.js" />
+          <img src="./imgs/vercel.png" width="128" alt="Vercel" title="Vercel" />
+          <img src="./imgs/netlify.png" width="128" alt="Netlify" title="Netlify" />
+          <img src="./imgs/strapi.png" width="128" alt="Strapi" title="Strapi" />
+          <img src="./imgs/react.png" width="128" alt="React" title="React" />
+          <img src="./imgs/supabase.png" width="128" alt="Supabase" title="Supabase" />
+          <img src="./imgs/tailwind-css.png" width="128" alt="Tailwind CSS" title="Tailwind CSS" />
+          <img src="./imgs/shadcn-ui.png" width="128" alt="Shadcn UI" title="Shadcn UI" />
+          <img src="./imgs/express-js.png" width="128" alt="Express.js" title="Express.js" />
+          <img src="./imgs/angular.png" width="128" alt="Angular" title="Angular" />
+          <img src="./imgs/docker.png" width="128" alt="Docker" title="Docker" />
+          {/* <img src="./imgs/html.png" alt="HTML" width="128" title="HTML" />
+          <img src="./imgs/css.png" alt="CSS" width="128" title="CSS" />
+          <img src="./imgs/javascript.png" alt="JS" width="128" title="JavaScript" />
+          <img src="./imgs/sass.png" width="128" alt="Sass" title="Sass" />
+          <img src="./imgs/react.png" width="128" alt="React" title="React" />
+          <img src="./imgs/nextjs.png" width="128" alt="Next JS" title="Next.js" />
+          
+          <img src="./imgs/python.png" width="128" alt="Python" title="Python" />
+          <img src="./imgs/html.png" alt="HTML" width="128" title="HTML" />
+          <img src="./imgs/css.png" alt="CSS" width="128" title="CSS" />
+          <img src="./imgs/javascript.png" alt="JS" width="128" title="JavaScript" />
+          <img src="./imgs/sass.png" width="128" alt="Sass" title="Sass" />
+          
+          
+          <img src="./imgs/azure.png" width="128" alt="Azure" title="Azure" />
+          <img src="./imgs/vscode.png" width="128" alt="VS Code" title="VS Code" />
+          <img src="./imgs/python.png" width="128" alt="Python" title="Python" /> */}
+        </div>
+      </div>
+    </section>
+  );
+}
