cd0fdc2aa3f849c8ee0c2855ad9a79b4b546e507
Khusroo Hayat <khusroo.hayat@gmail.com>
2025-11-01 11:08:35 +0500

    fix ci issues

--- app/Header.js
+++ app/Header.js
@@ -1,5 +1,5 @@
 'use client';
-import { useState } from 'react';
+import React, { useState } from 'react';
 
 export default function Header() {
   const [menuOpen, setMenuOpen] = useState(false);
--- app/layout.js
+++ app/layout.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import Head from 'next/head';
 import './globals.css';
 import Header from './Header';
